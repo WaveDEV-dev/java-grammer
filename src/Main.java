047f1a3b3fdab75d6f43b938a886656eb6289ae8
WaveDEV-dev <official@wavedev.kr>
2024-10-17 19:59:35 +0900

    메인 test3 삭제

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test1");
         System.out.println("test2");
-        System.out.println("test3");
     }
 }
