bf073d1b47f2e6257455fdc9a792a5c5e418c28e
WaveDEV-dev <official@wavedev.kr>
2024-10-29 19:54:31 +0900

    Main 업데이트

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
-        System.out.println("test2");
-
-        int a = 10;
-        int A = 20;
-        a = 12;
-
-        System.out.println(a);
-        System.out.println(A);
+        int localVariable = 10;
+        Integer number = 10;
+        System.out.println(localVariable);
+        System.out.println(number);
     }
 }
