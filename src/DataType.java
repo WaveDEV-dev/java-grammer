0fb0bf886e7d1a83417e1692c02b2b43d34134b0
WaveDEV-dev <official@wavedev.kr>
2024-10-29 19:06:46 +0900

    DataType 자료형 생성

---
+++ src/DataType.java
@@ -0,0 +1,37 @@
+public class DataType {
+
+    public static void main(String[] args) {
+
+        Sample instanceVariable1 = new Sample();
+        Sample instanceVariable2 = new Sample();
+
+        instanceVariable1.memberVariable = 10;
+        instanceVariable2.memberVariable = 20;
+        Sample.classVariable = 20;
+        instanceVariable1.sampleMethod(10);
+        instanceVariable2.sampleMethod(10);
+        System.out.println(instanceVariable1.memberVariable);
+        System.out.println(instanceVariable2.memberVariable);
+
+        int a = 1;
+        long b = 1;
+        float c = 0.1F;
+        double d = 0.1F;
+        boolean e = true;
+
+        char cat1 = 'c';
+        char cat2 = 'a';
+        char cat3 = 't';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        System.out.print(cat1);
+        System.out.print(cat2);
+        System.out.print(cat3);
+    }
+}
+
