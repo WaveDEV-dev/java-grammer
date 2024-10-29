2bd206c23318c3fa6f39d5e2b2013b963f5c108d
WaveDEV-dev <official@wavedev.kr>
2024-10-29 19:11:33 +0900

    Sample 변수 타입 작성

---
+++ src/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public int memberVariable;
+    static int classVariable;
+
+    public void sampleMethod(int parameterVariable) {
+        int localVariable = 10;
+        System.out.println(classVariable);
+    }
+}
