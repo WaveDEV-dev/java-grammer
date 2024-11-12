5845e40b7eb2887b5cf8aee257857c5a60452079
WaveDEV-dev <official@wavedev.kr>
2024-11-12 22:40:55 +0900

    Problem3 클래스 생성

---
+++ src/problem/Problem3.java
@@ -0,0 +1,17 @@
+package problem;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println("a = b");
+
+        int c = 10;
+        int d = 7;
+        System.out.println("c > d");
+
+        int e = 7;
+        int f = 10;
+        System.out.println("e < f");
+    }
+}
