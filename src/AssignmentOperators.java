871fab6a8dbc272e844f8608320512fc46e73920
WaveDEV-dev <official@wavedev.kr>
2024-11-12 19:57:37 +0900

    AssignmentOperators 작성 및 대입 연산자 연습

---
+++ src/AssignmentOperators.java
@@ -0,0 +1,18 @@
+// 대입 연산자 연습
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 20;
+        System.out.println("a = " + a);
+        a += 10;
+        System.out.println("a = " + a);
+        a -= 10;
+        System.out.println("a = " + a);
+        a *= 2;
+        System.out.println("a = " + a);
+        a /= 2;
+        System.out.println("a = " + a);
+        a %= 3;
+        System.out.println("a = " + a);
+
+    }
+}
