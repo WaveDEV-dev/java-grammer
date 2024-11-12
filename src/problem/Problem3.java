9fe1e2ed6f914524b1820d40d2e62ec28cd803fa
WaveDEV-dev <official@wavedev.kr>
2024-11-13 07:48:12 +0900

    Problem 3 클래스 출력 방식 변경

--- src/problem/Problem3.java
+++ src/problem/Problem3.java
@@ -4,14 +4,38 @@ public class Problem3 {
     public static void main(String[] args) {
         int a = 10;
         int b = 10;
-        System.out.println("a = b");
+        if (a == b) {
+            System.out.println("a = b");
+        }
+        if (a > b) {
+            System.out.println("a > b");
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
 
         int c = 10;
         int d = 7;
-        System.out.println("c > d");
+        if (c == d) {
+            System.out.println("c = d");
+        }
+        if (c > d) {
+            System.out.println("c > d");
+        }
+        if (c < d) {
+            System.out.println("c < d");
+        }
 
         int e = 7;
         int f = 10;
-        System.out.println("e < f");
+        if (e == f) {
+            System.out.println("e = f");
+        }
+        if (e > f) {
+            System.out.println("e > f");
+        }
+        if (e < f) {
+            System.out.println("e < f");
+        }
     }
 }
