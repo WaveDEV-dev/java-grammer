b7e2aa125ccdf843ae8eb86e1609784e64355880
WaveDEV-dev <official@wavedev.kr>
2024-11-13 07:47:39 +0900

    Problem2 클래스 Depth 최적화

--- src/problem/Problem2.java
+++ src/problem/Problem2.java
@@ -5,24 +5,39 @@ public class Problem2 {
         int score = 90;
         boolean diligence = true;
 
-        if(score >= 90) {
+        if(score >= 90 && diligence) {
             String scoreResult = "A";
-            if(diligence) {
-                System.out.println(scoreResult + "+");
-            }
+            System.out.println(scoreResult + "+");
+            return;
+        }
+        else if(score >= 90) {
+            String scoreResult = "A";
+            System.out.println(scoreResult);
+            return;
+        }
+
+        if(score >= 80 && diligence) {
+            String scoreResult = "B";
+            System.out.println(scoreResult + "+");
+            return;
         }
         else if(score >= 80) {
             String scoreResult = "B";
-            if(diligence) {
-                System.out.println(scoreResult + "+");
-            }
+            System.out.println(scoreResult);
+            return;
+        }
+
+        if(score >= 70 && diligence) {
+            String scoreResult = "C";
+            System.out.println(scoreResult + "+");
+            return;
         }
         else if(score >= 70) {
             String scoreResult = "C";
-            if(diligence) {
-                System.out.println(scoreResult + "+");
-            }
+            System.out.println(scoreResult);
+            return;
         }
+
         else {
             String scoreText = "F";
             System.out.println(scoreText);
