d2810703a46d516399aaf140ad75aa95aee9e5a6
WaveDEV-dev <official@wavedev.kr>
2024-11-13 18:27:30 +0900

    Problem6 클래스 작동 방식 변경
    LocalTime 패키지 -> Scanner 입력

--- src/problem/Problem6.java
+++ src/problem/Problem6.java
@@ -1,13 +1,29 @@
 package problem;
 
-import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Problem6 {
     public static void main(String[] args) {
-        LocalTime appointmentTime = LocalTime.now();
-        System.out.println("약속 시간: " + appointmentTime);
 
-        LocalTime appointmentTimeBefore = appointmentTime.plusMinutes(-45);
-        System.out.println("약속 시간으로부터 45분 전: " + appointmentTimeBefore);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("시간을 입력해주세요. (HH:mm 형식으로 입력): ");
+        String[] timeInput = sc.nextLine().split(":");
+
+        int hour = Integer.parseInt(timeInput[0]);
+        int minute = Integer.parseInt(timeInput[1]);
+
+        minute -= 45;
+
+        if (minute < 0) {
+            minute += 60;
+            hour -= 1;
+        }
+
+        if (hour < 0) {
+            hour += 24;
+        }
+
+        System.out.printf("약속 시간: %02d:%02d\n", hour, minute);
     }
 }
