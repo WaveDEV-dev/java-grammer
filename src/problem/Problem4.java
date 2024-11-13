ead3dad0c42f0ec5b727a3cfe34aa6bc6d87eb5b
WaveDEV-dev <official@wavedev.kr>
2024-11-13 18:16:05 +0900

    Problem4 클래스 Depth 최적화
    if문 Depth 최적화

--- src/problem/Problem4.java
+++ src/problem/Problem4.java
@@ -2,27 +2,24 @@ package problem;
 
 public class Problem4 {
     public static void main(String[] args) {
-        int x = -1;
+        int x = 1;
         int y = 1;
 
-        if(x > 0) {
-            if (y > 0) {
-                System.out.println("제1사분면에 속함");
-                return;
-            }
-            else {
-                System.out.println("제4사분면에 속함");
-                return;
-            }
+        if(x > 0 && y > 0) {
+            System.out.println("제1사분면에 속함");
+            return;
         }
-        if(x < 0) {
-            if (y > 0) {
-                System.out.println("제2사분면에 속함");
-                return;
-            } else {
-                System.out.println("제3사분면에 속함");
-                return;
-            }
+        if(x > 0 && y < 0) {
+            System.out.println("제4사분면에 속함");
+            return;
+        }
+        if(x < 0 && y > 0) {
+            System.out.println("제2사분면에 속함");
+            return;
+        }
+        if(x < 0 && y < 0) {
+            System.out.println("제3사분면에 속함");
+            return;
         }
     }
 }
