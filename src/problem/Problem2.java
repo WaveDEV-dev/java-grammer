ee4d7368670d36dc006b98e040d70e95d8beca3a
WaveDEV-dev <official@wavedev.kr>
2024-11-14 19:26:35 +0900

    Problem2 클래스 리팩토링

--- src/problem/Problem2.java
+++ src/problem/Problem2.java
@@ -4,44 +4,37 @@ public class Problem2 {
     public static void main(String[] args) {
         int score = 90;
         boolean diligence = true;
+        String scoreResult = "";
 
         if(score >= 90 && diligence) {
-            String scoreResult = "A";
-            System.out.println(scoreResult + "+");
-            return;
+            scoreResult = "A";
         }
-        else if(score >= 90) {
-            String scoreResult = "A";
-            System.out.println(scoreResult);
-            return;
+        else if(score >= 80 && diligence) {
+            scoreResult = "B";
+        }
+        else if(score >= 70 && diligence) {
+            scoreResult = "C";
         }
 
-        if(score >= 80 && diligence) {
-            String scoreResult = "B";
+        if(diligence) {
             System.out.println(scoreResult + "+");
-            return;
+        }
+
+        if(score >= 90) {
+            scoreResult = "A";
         }
         else if(score >= 80) {
-            String scoreResult = "B";
-            System.out.println(scoreResult);
-            return;
-        }
-
-        if(score >= 70 && diligence) {
-            String scoreResult = "C";
-            System.out.println(scoreResult + "+");
-            return;
+            scoreResult = "B";
         }
         else if(score >= 70) {
-            String scoreResult = "C";
-            System.out.println(scoreResult);
-            return;
+            scoreResult = "C";
+        }
+        else {
+            scoreResult = "F";
         }
 
-        else {
-            String scoreText = "F";
-            System.out.println(scoreText);
-            return;
+        if(diligence == false) {
+            System.out.println(scoreResult);
         }
     }
 }
