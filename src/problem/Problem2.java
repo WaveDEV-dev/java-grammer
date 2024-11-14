265fac77fdb837011507cb70f61fec7b1a22f205
WaveDEV-dev <official@wavedev.kr>
2024-11-14 19:46:57 +0900

    Problem2 클래스 리팩토링
    +학점 설정에 쓰이는 대입 연산자 추가 및 if문 분기 최적화

--- src/problem/Problem2.java
+++ src/problem/Problem2.java
@@ -2,24 +2,12 @@ package problem;
 
 public class Problem2 {
     public static void main(String[] args) {
-        int score = 90;
-        boolean diligence = true;
+        // 변수 선언
+        int score = 91;
+        boolean diligence = false;
         String scoreResult = "";
 
-        if(score >= 90 && diligence) {
-            scoreResult = "A";
-        }
-        else if(score >= 80 && diligence) {
-            scoreResult = "B";
-        }
-        else if(score >= 70 && diligence) {
-            scoreResult = "C";
-        }
-
-        if(diligence) {
-            System.out.println(scoreResult + "+");
-        }
-
+        // 학점 변수 설정
         if(score >= 90) {
             scoreResult = "A";
         }
@@ -33,8 +21,13 @@ public class Problem2 {
             scoreResult = "F";
         }
 
-        if(diligence == false) {
-            System.out.println(scoreResult);
+        // +학점 설정
+        if(diligence && !scoreResult.equals("F")) {
+            scoreResult += "+";
         }
+
+        // 결과문 출력
+        System.out.println(scoreResult);
+
     }
 }
