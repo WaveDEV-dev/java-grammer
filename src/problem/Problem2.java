91080901547a357852f5c7e7737f49fb9aff8be9
WaveDEV-dev <official@wavedev.kr>
2024-11-14 20:39:21 +0900

    Problem2 클래스 메서드 생성
    setPlusGradeVariable 메서드 생성

--- src/problem/Problem2.java
+++ src/problem/Problem2.java
@@ -1,37 +1,40 @@
 package problem;
 
 public class Problem2 {
-    boolean isDiligence = false;
+    boolean isDiligence = true;
     boolean isF = false;
+
     // 학점 변수 설정
-    public static String setGradeVariable(int score) {
-        if(score >= 90) {
+    public String setGradeVariable(int score) {
+        if (score >= 90) {
             return "A";
-        }
-        else if(score >= 80) {
+        } else if (score >= 80) {
             return "B";
-        }
-        else if(score >= 70) {
+        } else if (score >= 70) {
             return "C";
-        }
-        else {
-            return "F";
+        } else {
             this.isF = true;
+            return "F";
         }
     }
+    public String setPlusGradeVariable(String scoreResult) {
+        if (isDiligence && !isF) {
+            scoreResult += "+";
+        }
+        return scoreResult;
+    }
     public static void main(String[] args) {
-        // 조건 변수 선언
+        // 클래스 선언
+        Problem2 problem = new Problem2();
         // 변수 선언
-        int score = 91;
+        int score = 100;
         String scoreResult = "";
 
         // 학점 변수 설정
-        setGradeVariable(score);
+        scoreResult = problem.setGradeVariable(score);
 
         // +학점 설정
-        if(isDiligence && isF) {
-            scoreResult += "+";
-        }
+        scoreResult = problem.setPlusGradeVariable(scoreResult);
 
         // 결과문 출력
         System.out.println(scoreResult);
