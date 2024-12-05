1fea0a5cebd8005addeb4b12812b22862bf152fa
WaveDEV-dev <official@wavedev.kr>
2024-12-05 19:07:12 +0900

    Problem8 클래스 리팩토링
    클래스 내부 while문의 함수화

--- src/problem/Problem8.java
+++ src/problem/Problem8.java
@@ -21,11 +21,25 @@ public class Problem8 {
         }
         return result;
     }
+
+    public static int findEvenWithWhile(int[] find) {
+        int arrayInt = 0;
+        int result = 0;
+        while(arrayInt < find.length) {
+
+            if(find[arrayInt] % 2 == 0) {
+                result++;
+            }
+
+            arrayInt++;
+        }
+        return result;
+    }
     public static void main(String[] args) {
         // 배열에서 짝수의 개수 찾기
         // 배열 {1, 2, 3, 4, 5, 6, 8, 9}가 주어졌다면, 짝수는 2, 4, 6, 8로 총 4개
-//        int[] myArray = {2, 2, 4, 4, 6, 6, 7, 8, -2};
-        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] myArray = {2, 2, 4, 4, 6, 6, 7, 8, -2};
+//        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 //        int[] myArray = {2, 4, 6, 8, 10, 12};
 //        int[] myArray = {1, 3, 5, 7, 9};
 //        int[] myArray = {-4, -3, -2, -1, 0, 1, 2, 3, 4};
@@ -34,40 +48,9 @@ public class Problem8 {
         int result = 0;
 
 //        result = findEvenWithForArrayLoop(myArray);
-        result = findEvenWithFor(myArray);
+//        result = findEvenWithFor(myArray);
+        result = findEvenWithWhile(myArray);
 
-
-//        for(int number : myArray){
-//            System.out.print( number + " ");
-//        }
-//        System.out.println();
-//
-//
-//        for(int arrayInt = 0; arrayInt < myArray.length; arrayInt++){
-//            System.out.print(myArray[arrayInt] + " ");
-//        }
-//        System.out.println();
-
-
-
-
-
-
-//        for(int arrayInt = 0; arrayInt < myArray.length; arrayInt++){
-//            if(myArray[arrayInt] % 2 == 0) {
-//                result++;
-//            }
-//        }
-
-        // 배열의 길이만큼 반복
-//        while(arrayInt < myArray.length) {
-//
-//            if(myArray[arrayInt] % 2 == 0) {
-//                result++;
-//            }
-//
-//            arrayInt++;
-//        }
 
         System.out.println("짝수의 총합은 " + result + "개입니다.");
     }
